1020c5402067c12bb779a2892ca5326c74ad76b1
remmi755 <90457721+remmi755@users.noreply.github.com>
2022-11-20 21:07:18 +0300

    create card and add cartEmpty

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,9 @@ import Search from './Search'
 
 function Header() {
     const { items, totalPrice} = useSelector(state => state.cart)
+
+    const totalCount = items.reduce((sum, item) => sum + item.count, 0)
+
     return (
         <div className="header">
             <div className="container">
@@ -58,7 +61,7 @@ function Header() {
                                 strokeLinejoin="round"
                             />
                         </svg>
-                        <span>{items.length}</span>
+                        <span>{totalCount}</span>
                     </Link>
                 </div>
             </div>
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -21,17 +21,24 @@ export const cartSlice = createSlice({
             }
 
 
-            state.totalPrice = state.items.reduce((sum, obj) => obj.price + sum, 0)
+            state.totalPrice = state.items.reduce((sum, obj) => (obj.price * obj.count) + sum, 0)
+        },
+        minusItem: (state, action) => {
+            const findItem = state.items.find((obj) => obj.id === action.payload)
+            if(findItem) {
+                findItem.count --
+            }
         },
         removeItem: (state, action) => {
             state.items = state.items.filter((obj) => obj.id !== action.payload)
         },
         clearItems: (state) => {
             state.items = []
+            state.totalPrice = 0
         },
     },
 })
 
-export const { addItem, removeItem, clearItems } = cartSlice.actions
+export const { addItem, removeItem, clearItems, minusItem } = cartSlice.actions
 
 export default cartSlice.reducer
