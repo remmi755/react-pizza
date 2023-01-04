168a1e67dd383e5bb54e0c7c3500a7189a22e6da
remmi755 <90457721+remmi755@users.noreply.github.com>
2023-01-04 21:56:58 +0300

    add changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import {HashRouter} from 'react-router-dom';
 
 import App from './App'
 import {store} from './redux/store'
@@ -11,11 +12,11 @@ const rootElement = document.getElementById('root')
 if(rootElement) {
     const root = ReactDOM.createRoot(rootElement)
     root.render(
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
