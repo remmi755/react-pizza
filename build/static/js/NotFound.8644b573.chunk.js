f64bd7aace937a26cf7340f0b4c383afc0a94a52
remmi755 <90457721+remmi755@users.noreply.github.com>
2023-01-05 20:13:47 +0300

    Updates

--- build/static/js/NotFound.8644b573.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[187],{4949:function(e,r,a){a.r(r);a(2791);var n=a(1140),t=a(184);r.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)(n.vr,{})})}}}]);
-//# sourceMappingURL=NotFound.8644b573.chunk.js.map
